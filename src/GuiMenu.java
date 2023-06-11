ee4280660ab5ff5f66f9c02de0bac8aaafdf0f39
tomer00gud <himanshutomer00@gmail.com>
2023-06-11 23:27:18 +0530

    initial commit

--- src/GuiMenu.java
+++ src/GuiMenu.java
@@ -3,6 +3,11 @@ import javax.swing.*;
 public class GuiMenu extends JFrame {
 
     private JPanel root;
+    private JButton btStart;
+    private JPanel pRed;
+    private JPanel pGreen;
+    private JPanel pYellow;
+    private JPanel pBlue;
 
 
     public GuiMenu(){
